b5d200e62448b502ba7211157f35881b73b8bda8
rapando <rapando>
2020-10-24 23:15:37 +0300

    working on ch9

---
+++ ch9/src/main.rs
@@ -0,0 +1,41 @@
+/*
+ * Error Handling
+ */
+use std::fs::File;
+// use std::io::ErrorKind;
+
+fn main() {
+    // let v = vec![1,2,3];
+    // v[12];
+
+
+
+    // option 1
+    // let f = File::open("hello.txt");
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    // option 2
+    // let _f = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {:?}", error);
+    //         })
+    //     } else {
+    //         panic!("Problem opening the file: {:?}", error);
+    //     }
+    // });
+
+    // option 3
+    let _f = File::open("hello.txt").expect("Failed to open file");
+}
