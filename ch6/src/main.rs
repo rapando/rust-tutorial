a1d07c75c7ad79804439cb25459ada7a4bce61d9
rapando <rapando>
2020-10-24 14:58:32 +0300

    simple flask app

--- ch6/src/main.rs
+++ ch6/src/main.rs
@@ -1,3 +1,36 @@
+/*
+ * Enums and Pattern Matching
+*/
+
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("Home      : {:?}", home);
+    println!("Loopback  : {:?}", loopback);
+
+    let some_u8_value = 2u8;
+    match some_u8_value {
+        1 => println!("One"),
+        2 => println!("Two"),
+        _ => (),
+    };
+
+    println!("");
+
+    let some_u8_value = Some(3u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    };
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }
